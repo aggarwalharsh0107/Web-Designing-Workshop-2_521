548218f49c96c98faf3a40dcf96e71642d67a05d
aggarwalharsh0107 <https://github.com/aggarwalharsh0107/Web-Designing-Workshop-2_521.git>
2026-04-07 16:07:08 +0530

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# Web-Designing-Workshop-2_521
-<p>Harsh Aggarwal is a good boy</p>
-<br>
-<p>He is from muzaffarnagar</p>
---
+++ java script/java fundamentals/app.js
@@ -0,0 +1,25 @@
+// var name="John Doe";
+// var age=30;
+// var isEmployed= true;
+// console.log(name);
+// console.log(age);
+// console.log(isemployed);
+// let age=30;
+// console.log(age);
+
+// if(true){
+//     var age=30;
+// }
+// console.log(age);
+
+// const PI=3.14;
+// console.log(PI);
+// PI= 3.14159; //this will cause an error
+// console.log(PI);
+//consatnt variable can not be reassigned .
+//type of operator
+
+let name = "Priya"
+let age1 = 20;
+console.log("my name is" + name + "and my age is " + age1);
+console.log(`My name is ${name} and my age is ${age1}`)
