aab5fa9a1bdc56d5d996aad3b550c021f7d368e7
aggarwalharsh0107 <https://github.com/aggarwalharsh0107/Web-Designing-Workshop-2_521.git>
2026-05-16 16:40:00 +0530

    commit by aggarwal

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css"
+import Title from "./Title.jsx"
+import ProductTab from "./ProductTab.jsx"
+
+
+
+function Desciption() {
+  return <h1>I am the Desciption</h1>
+}
+
+function App() {
+ return <ProductTab/>
+}
+
+export default App;
+
+// function App() {
+//  return (
+//   <div>
+//     <h1>This is my app component</h1>
+//     <p>inside app component we have :</p>
+//     <Title/>
+//     <Desciption/>
+//     <title/>
+//   </div>
+
+
+// function App() {
+//  return (
+//   <>
+//     <h1>This is my app component</h1>
+//     <p>inside app component we have :</p>
+//     <Title/>
+//     <Desciption/>
+//     <title/>
+//   </>
+
+//  );
+// }
+
+// function App() {
+//  return (
+//   <>
+//     <Product/>
+//     <Product/>
+//     <Product/>
+//   </>
+
+//  );
+// }
+
+// function App() {
+//  return <ProductTab/>
+// }
+
+// export default App;
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,15 @@
+
+// function Title() {
+//   return <h1>I am the Title!</h1>
+// }
+
+function Title() {
+  return (
+    <div>
+        <p>2 * 2 = {2 * 2}</p>
+        <p>Hi,{name.toUpperCase()}</p>
+    </div>
+  )
+}
+
+export default Title;
