d768dc2adf9d0c156e3f3600bcca809ab74b926a
aggarwalharsh0107 <https://github.com/aggarwalharsh0107/Web-Designing-Workshop-2_521.git>
2026-06-20 11:47:38 +0530

    Replace react-currency-format with react-number-format

--- amazon_clone-main/src/Subtotal.js
+++ amazon_clone-main/src/Subtotal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Subtotal.css";
-import CurrencyFormat from "react-currency-format";
+import { NumericFormat } from "react-number-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
 function Subtotal() {
