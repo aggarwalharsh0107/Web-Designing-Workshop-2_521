db7d44683bde8f4d380a8f8e0cbabb5b51dc681f
aggarwalharsh0107 <https://github.com/aggarwalharsh0107/Web-Designing-Workshop-2_521.git>
2026-06-29 22:33:51 +0530

    complete practice question of lecture 4 of shradha didi

--- javascript/javafundamentals/app.js
+++ javascript/javafundamentals/app.js
@@ -720,6 +720,64 @@ while(true) {
     req = prompt("please enter your request");
 }
 
+// practice question based on lecture 4
+// practice question 1 
+let arr = [1,2,3,4,5,6,2,3];
+let num = 2;
+
+for(let i=0; i<arr.length; i++) {
+    if (arr[i] == num) {
+        arr.splice(i, 1);
+    }
+}
+console.log(arr);
+
+// practice question 2 
+let number = 287152;
+let count = 0;
+
+let copy = number;
+while(copy > 0) {
+    count++;
+    copy = Math.floor(copy/10);
+}
+
+console.log(count);
+
+// practice question 3  sum of a digit 
+let number = 287152;
+let sum = 0;
+let copy = number;
+
+while(copy>0){
+    digit = copy % 10;
+    sum+= digit;
+    copy = Math.floor(copy/10);
+}
+console.log(sum);
+
+// practice question 4 factorial n
+let n = 5;
+let factorial = 1;
+
+for(let i=1; i<=n; i++) {
+    factorial *= i;
+}
+console.log(`factorial of ${n} is ${factorial}`);
+
+// practice  question 5 find the largest number
+let arr = [2,5,10,4,2,7,1,9];
+let largest = 0;
+
+for(let i=0; i<=arr.length; i++) {
+    if(largest < arr[i]) {
+        largest = arr[i];
+    }
+}
+console.log(largest);
+
+
+
 
 
 
